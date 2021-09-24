a750d3b74eda656a372d17134c07df8fa5907397
J Leadbetter <j.leadbetter@circleci.com>
2021-09-24 09:08:15 +0200

    Base leiningen project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to embedded-spoons
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/embedded_spoons/core.clj
@@ -0,0 +1,7 @@
+(ns embedded-spoons.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
